46db0c64f31137a9f57e3f1e053dc318d8294c8f
Lucas Rose <137231501+jedibguylu@users.noreply.github.com>
2026-09-21 15:45:22 -0500

    Create calcSpeed.ino and original write
    Signed-off-by: Lucas Rose <137231501+jedibguylu@users.noreply.github.com>

---
+++ boatCode/calcSpeed.ino
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+double calcDist(double boatX, double boatY, double targetX, double targetY){
+    double xDist = targetX - boatX;
+    double yDist = targetY - boatY;
+    double distance = std::sqrt((xDist * xDist) + (yDist * yDist));
+    return distance;
+}
+
+int main()
+{
+    std::cout << calcDist(0, 0, 0.5, std::sqrt(3)/2);
+    return 0;
+}
