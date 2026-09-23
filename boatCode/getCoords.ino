17f2168cb59418984f56ce78433783293afc9ee7
Lucas Rose <137231501+jedibguylu@users.noreply.github.com>
2026-09-22 21:15:22 -0500

    Update getCoords.ino
    Made pseudo code
    
    Signed-off-by: Lucas Rose <137231501+jedibguylu@users.noreply.github.com>

--- boatCode/getCoords.ino
+++ boatCode/getCoords.ino
@@ -1,3 +1,8 @@
 void getCoords() {
-
+  /**
+  Do whatever the code examples and documentation does to get a clean coordinate output. I dont want letter (like N or W), 
+  just clean decimal values like (31.2345435, 45.324234). This makes the math later much easier. 
+  Idk how you want to get the two values out, but figure it out!
+  Make it as precise as you can without further testing.
+  **/
 }
