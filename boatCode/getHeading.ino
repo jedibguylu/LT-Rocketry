adbe263e2e58b6a490ec49a776f8b405c8b8bf54
Lucas Rose <137231501+jedibguylu@users.noreply.github.com>
2026-09-22 21:17:04 -0500

    Update getHeading.ino
    Created pseudo code
    
    Signed-off-by: Lucas Rose <137231501+jedibguylu@users.noreply.github.com>

--- boatCode/getHeading.ino
+++ boatCode/getHeading.ino
@@ -1,1 +1,6 @@
-
+getHeading() {
+  /**
+  Should use the compass to get >>>>>>>>> TRUE <<<<<<<<< NOT MAGNETIC heading. Ideally just put it as an int out of 360.
+  Make it as precicse as posible. Looks like the code example in the doc did pretty well.
+  **/
+}
