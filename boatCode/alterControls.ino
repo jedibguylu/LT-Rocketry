1a98619b5c2c2f85a56b7b05ca2104737e78831b
Lucas Rose <137231501+jedibguylu@users.noreply.github.com>
2026-09-22 21:13:10 -0500

    Update alterControls.ino
    Made pseudo code
    
    Signed-off-by: Lucas Rose <137231501+jedibguylu@users.noreply.github.com>

--- boatCode/alterControls.ino
+++ boatCode/alterControls.ino
@@ -1,3 +1,11 @@
 void alterControls(){
-
+  /**
+  Using the differental thrusters, the code should increase left or right motor respectively to swing the nose
+  in the direction of calcAngle() until that angle is 0.
+  
+  The code should also keep going until the boat gets to the point with calcDist() == 0.
+  Maybe it should also slow down when getting near the point and there should be some tolerances (whatever the GPS datasheet
+  says the minimum tolerance is is what we should use or itll just spin around the point it cant get to)
+  Maybe just have it run-ashore idk.
+  **/
 }
