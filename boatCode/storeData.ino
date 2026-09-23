0597ec957ad6668596b00a4958ec8d9105ff2c8d
Lucas Rose <137231501+jedibguylu@users.noreply.github.com>
2026-09-22 21:18:45 -0500

    Update storeData.ino
    Created pseudo code
    
    Signed-off-by: Lucas Rose <137231501+jedibguylu@users.noreply.github.com>

--- boatCode/storeData.ino
+++ boatCode/storeData.ino
@@ -1,3 +1,6 @@
 void storeData(){
-
+  /**
+  Just needs to open a new and CLEARLY LABLED file on startup and log all raw and calculated values as well as what the motors
+  do ever couple millis. If someone wants to make a general outline for this I can talior it later to the needs.
+  **/
 }
